7a2b9613554c2d95cc4d955d15a8c9d234e1ee0f
Егор Лазуткин <aovtar74@gmail.com>
2022-10-14 12:47:23 +0300

    insert added

---
+++ insert.sql
@@ -0,0 +1,115 @@
+insert into albums values
+	(1, 'Dynasty', 1979),
+	(2, 'Bandana', 2021),
+	(3, 'Sexy Drill', 2021),
+	(4, 'Sempiternal', 2013),
+	(5, 'Astroworld', 2018),
+	(6, 'Karmageddon', 2019),
+	(7, 'Future Nostalgia', 2020),
+	(8, 'Моё Русское Сердце', 1996),
+	(9, 'Kamikaze', 2018);
+
+Insert into perfomers values
+	(1, 'Kiss'),
+	(2, 'Kizaru'),
+	(3, 'Big Baby Tape'),
+	(4, 'Travis Scott'),
+	(5, 'Bring Me The Horizon'),
+	(6, 'Og Buda'),
+	(7, 'Dua Lipa'),
+	(8, 'Татьяна Буланова'),
+	(9, 'Eminem');  
+
+Insert into genres values
+	(1, 'trap'),
+	(2, 'rock'),
+	(3, 'heavy-metal'),
+	(4, 'Drill'),
+	(5, 'pop-music'),
+	(6, 'hip-hop');  
+
+Insert into albumperfomer values
+	(1, 1),
+	(2, 2),
+	(2, 3),
+	(3, 6),
+	(4, 5),
+	(5, 4),
+	(6, 2),
+	(7, 7),
+	(8, 8),
+	(9, 9);
+
+Insert into genreperfomer values
+	(1, 2),
+	(1, 3),
+	(1, 4),
+	(2, 1),
+	(3, 1),
+	(2, 5),
+	(4, 6),
+	(5, 7),
+	(5, 8),
+	(6, 9);
+
+Insert into tracks values
+	(1, 1, 271, 'I Was Made For Lovin You'),
+	(2, 2, 192, 'Big Tymers'),
+	(3, 2, 135, 'Million'),
+	(4, 3, 217, 'Трафик'),
+	(5, 4, 227, 'Can You Feel My Heart'),
+	(6, 7, 204, 'Levitating'),
+	(7, 7, 222, 'Break My Heart'),
+	(8, 7, 194, 'Physical'),
+	(9, 6, 273, 'Deep End'),
+	(10, 6, 174, 'Дежавю'),
+	(11, 5, 191, 'Butterfly Effect'),
+	(12, 5, 271, 'Stargazing'),
+	(13, 8, 209, 'Ясный мой свет'),
+	(14, 9, 338, 'The Ringer'),
+	(15, 9, 342, 'Normal'),
+	(16, 9, 270, 'Venom');
+
+insert into collections values
+	(1, 'Хиты прошлых лет', 2010),
+	(2, 'Speech Technique', 2011),
+	(3, 'New school', 2022),
+	(4, 'Russian song', 2019),
+	(5, 'Trap Sound', 2022),
+	(6, 'Rock Band', 2014),
+	(7, 'Pop Vibe', 2022),
+	(8, 'Mix For Chill', 2019),
+	(9, 'Cheerful mix', 2020);
+
+insert into collectiontrack values
+	(1, 1),
+	(1, 13),
+	(2, 10),
+	(2, 14),
+	(2, 15),
+	(3, 2),
+	(3, 3),
+	(4, 4),
+	(4, 10),
+	(4, 13),
+	(5, 2),
+	(5, 3),
+	(5, 9),
+	(5, 10),
+	(5, 11),
+	(5, 12),
+	(6, 1),
+	(6, 5),
+	(7, 6),
+	(7, 7),
+	(7, 8),
+	(7, 13),
+	(8, 1),
+	(8, 5),
+	(8, 11),
+	(8, 12),
+	(8, 16),
+	(9, 9),
+	(9, 10),
+	(9, 14),
+	(9, 15);
